b11e07e5f9c97e4981d2b6c2db944fd6aab26c8a
Sanchit-Warang <sanchit.w@somaiya.edu>
2022-01-15 18:03:28 +0530

    getweather added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-
+import  Axios  from 'axios';
 function App() {
+  const getWeather = () => {
+    Axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=mumbai&appid=8088aab0992fb7d20014538ca693715c`).then((response)=>{
+      console.log(response)
+      const dailyData = response.data.list.filter(reading => {   
+        return reading.dt_txt.includes("18:00:00") 
+        }
+      )
+      console.log(dailyData) 
+    })
+  }
   return (
     <div className="App">
       
